e7adb357f2f40e349d027b357cc215b23454ed2c
DevodaiDaGoat <devodaidivu@gmail.com>
2026-04-18 17:17:52 +0000

    fix

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -11,7 +11,7 @@ sudo rm -rf /tmp/.X11-unix/X1
 
 # 2. START VIRTUAL DISPLAY (Lower depth = Much higher speed)
 # Using 16-bit color depth (x16) instead of 24-bit makes it 40% faster.
-Xvfb :1 -screen 0 1366x768x16 +extension RANDR +extension RENDER +extension GLX &
+Xvfb :0 -screen 0 1366x768x16 +extension RANDR +extension RENDER +extension GLX &
 sleep 2
 
 # 3. START WINDOW MANAGER (Openbox is tiny and fast)
@@ -21,7 +21,7 @@ sleep 1
 # 4. START VNC (Optimized flags for lag)
 # -ncache 10 caches pixels on your side to reduce internet lag
 # -noxdamage stops redundant screen refreshes
-x11vnc -display :1 -forever -nopw -shared -rfbport 5900 -noxdamage -ncache 10 &
+x11vnc -display :0 -forever -nopw -shared -rfbport 5900 -noxdamage -ncache 10 &
 
 # 5. START WEB BRIDGE
 websockify --web=/usr/share/novnc 6080 localhost:5900 &
